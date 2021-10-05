9401cf7c65a968b2db5fdf829994d348b21e9dfd
Andrew Faria (contractor) <andrew.faria@macys.com>
2021-10-05 17:31:11 -0400

    Delete boilerplate

--- src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Logo } from "./Logo";
-export { default as ResponsiveDrawer } from "./ResponsiveDrawer";
-export { default as ResponsiveLayout } from "./ResponsiveLayout";
--- src/hooks/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as useToggle } from "./useToggle";
-export { default as usePopover } from "./usePopover";
-export { default as useBreakpoint } from "./useBreakpoint";
